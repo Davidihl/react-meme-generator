fe8ac2555d434e3b8543a3d7f5eff2a185782bbb
David Ihl <daih1985@gmail.com>
2023-05-08 13:32:41 +0200

    Add basic PWA support

---
+++ public/serviceWorker.js
@@ -0,0 +1,56 @@
+// STORAGE OF BROWSER
+
+const CACHE_NAME = 'version-1';
+const urlsToCache = [
+  'index.html',
+  'offline.html',
+  '../src/App.js',
+  '../src/App.module.css',
+  '../src/index.js',
+  '../src/index.css',
+  '../src/components/Form.js',
+  '../src/components/Form.module.css',
+  '../src/components/Meme.js',
+  '../src/components/Meme.module.css',
+  '../src/components/MemeHistory.js',
+  '../src/components/MemeHistory.module.css',
+];
+
+// installation
+this.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+// listen for request
+this.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match('index.html'));
+    }),
+  );
+});
+
+// Activate the service worker
+this.addEventListener('activate', (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          } else {
+            return cacheName;
+          }
+        }),
+      ),
+    ),
+  );
+});
